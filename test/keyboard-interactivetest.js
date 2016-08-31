60d728845f9418ea2f83d5ee92f5b3c4564bbdb3
Andrew McMurtrie <cmp-202@anyzoom.com>
2016-08-31 19:04:46 +1200

    keyboard-interactive and timeout test files
    Separated out timeout content from the keyboard-interactive test file to
    the timeout test file.

--- test/keyboard-interactivetest.js
+++ test/keyboard-interactivetest.js
@@ -21,23 +21,7 @@ var host = {
   }, 
   idleTimeOut: 3000,
   verbose: false,
-  debug: true,
-  onCommandProcessing: function( command, response, sshObj, stream ){
-   //Use onCommandProcessing to handle non-standard prompts from keyboard-interactive if you keep on getting onCommandTimeout events
-   //You might have to try the following where "Connected on port 22" is the known response from the server prior to requiring `enter` to proceed
-   //Remember this event triggers for every char coming from the server not when it all finishes.
-   /*if ( command === "" && response.indexOf("Connected on port 22") != -1 ){
-      if (sshObj.debug){sshObj.msg.send(sshObj.server.host + ": Keyboard-interactive connected");}
-       //In some cases \n doesn't seem to be responded to by the server try \r\n or \r
-       //stream.write("\n");
-   }*/
-  },
-  onCommandTimeout: function( command, response, stream, connection ){
-    //Attaching an event handler to the instance will run in parrallel to the default handler
-    //The host handler replaces the default handler so we set this to nothing making the 
-    //instance definition the primary handler.
-    //I did not define the handler here because access is required to the instance 'this' properties and functions 
-  },
+  debug: false,
   onEnd: function( sessionText, sshObj ){
     sshObj.msg.send (sessionText);
   }
@@ -60,43 +44,8 @@ SSH.on ('keyboard-interactive', function(name, instructions, instructionsLang, p
        var str = JSON.stringify(prompts, null, 4);
        this.emit('msg', "Prompts object: " + str);
      }
+     //if only the password is required then it will be the only thing returned in the array
      finish([this.sshObj.server.password] );
-  });
-  
-SSH.on ('commandTimeout', function( command, response, stream, connection ){
-   //Use onCommandTimeout to handle keyboard-interactive not providing a standard prompt on connection
-   //If onCommandProcessing stream.write("\n") failed and nothing else works there might be nothing you can do here
-   //If there is a delay after the last char was sent from the server before it will accept input onCommandProcessing might not work
-   //The following handles a second attemp at getting a standard prompt. If that fails (no data response from host)
-   //then a second timeout timer is set to stop the script hanging. The final code handles command or keyboard-interactive error response.
-   var errorMessage, errorSource;
-   if(this.sshObj.debug){this.emit("msg","timeout");}
-   //first keyboard-interactive attemp we know there is no command and we set a flag sshObj.sentN after the first attemp
-   if ( command === "" && this.sshObj.sentN != true){
-     if(this.sshObj.debug){this.emit("msg","Keyboard-interactive timeout first pass");}
-     //first attemp so set the flag we will use to ignor another timeout
-     this.sshObj.sentN = true
-     if(this.sshObj.debug){this.emit("msg","new timmer");} 
-     //reset the timeout timer
-     clearTimeout(this.sshObj.idleTimer);
-     this.sshObj.idleTimer = setTimeout(function(){
-         this.emit('commandTimeout', command, response, stream, connection )
-     }, this._idleTime);
-     //send whatever is required to trigger a response
-     stream.write("\n");
-     //we want to skip the last part     
-     return true;
-   } else if (command === "" && this.sshObj.sentN === true){
-     if(this.sshObj.debug){this.emit("msg","Keyboard-interactive timeout second pass");}
-     //second failure so we set the error messages
-     errorMessage = "keyboarb-interactive prompt"
-     errorType = "keyboarb-interactive Timeout";
-   } 
-   //everything failed so update sessionText and raise an error event that closes the connection
-   this.sshObj.sessionText += response;
-   if(!errorMessage){errorMessage = "Command";}
-   if(!errorSource){errorSource = "Command Timeout";}
-   this.emit("error", this.sshObj.server.host + ": " + errorMessage + " timed out after " + (this._idleTime / 1000) + " seconds", errorSource, true);   
   });
   
 SSH.connect();
--- test/timeouttest.js
+++ test/timeouttest.js
@@ -25,6 +25,13 @@ var sshObj = {
   connectedMessage:   "Connected",
   readyMessage:       "Running commands Now",
   closedMessage:      "Completed",
+  onCommandTimeout: function( command, response, stream, connection ){
+    //The host handler replaces the default handler so we set this to nothing making the 
+    //instance definition the primary handler.
+    //Attaching an event handler to the instance will run in parrallel to the default handler that is why we set this one to do nothing
+    //I did not define the handler here because access is required to the instance 'this', sshObj and emitters
+    //which are not available here.    
+  },
   onEnd:              function( sessionText, sshObj ) {
     //show the full session output. This could be emailed or saved to a log file.
     sshObj.msg.send("\nThis is the full session responses:\n" + sessionText);
@@ -35,4 +42,46 @@ var SSH2Shell = require ('../lib/ssh2shell');
 
 //run the commands in the shell session
 var SSH = new SSH2Shell(sshObj);
+
+SSH.on ('commandTimeout', function( command, response, stream, connection ){
+   //Here we are trying to handle a timeout from not getting a standard prompt from the host.
+   //The first check makes sure there is no command and the first try flag has not been set.   
+   //a second timeout timer is set to stop the script hanging.
+   //If that fails (no data response from host) then error messages are set. 
+   //The final code adds the text received so far to the session text and closes the connection with an error.
+   var errorMessage, errorSource;
+   if(this.sshObj.debug){this.emit("msg","timeout");}
+   //first we are checking for the timeout coming after connection before a prompt is detected and before a command is loaded
+   //on the first try this.sshObj.sentN is not true as it hasn't been set yet
+   if ( command === "" && this.sshObj.sentN != true){
+     if(this.sshObj.debug){this.emit("msg","Keyboard-interactive timeout first pass");}
+     //first attemp so set the flag we will use to ignor another timeout attempt
+     this.sshObj.sentN = true
+     if(this.sshObj.debug){this.emit("msg","new timmer");} 
+     //reset the timeout timer to catch a timeout from sending \n
+     clearTimeout(this.sshObj.idleTimer);
+     this.sshObj.idleTimer = setTimeout(function(){
+         this.emit('commandTimeout', command, response, stream, connection )
+     }, this._idleTime);
+     //send whatever is required to trigger a response
+     stream.write("\n");
+     //we want to skip the last part so return     
+     return true;
+   } else if (command === "" && this.sshObj.sentN === true){
+     if(this.sshObj.debug){this.emit("msg","timeout second pass");}
+     //second failure so we set the error messages because we probably can't do anything more
+     //or add code to try something else 
+     errorMessage = "keyboarb-interactive prompt"
+     errorType = "keyboarb-interactive Timeout";
+   } //else if ( command === "some-command" and response.indexOf("who am I?") != -1){
+       //This would be better to handle in onCommandProcessing but can be handled here
+       //Do something to respond to the timeout or bail to the error.
+   //}
+   //everything failed so update sessionText and raise an error event that closes the connection
+   this.sshObj.sessionText += response;
+   if(!errorMessage){errorMessage = "Command";}
+   if(!errorSource){errorSource = "Command Timeout";}
+   this.emit("error", this.sshObj.server.host + ": " + errorMessage + " timed out after " + (this._idleTime / 1000) + " seconds", errorSource, true);   
+  });
+  
 SSH.connect();
