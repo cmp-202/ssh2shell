89d409b9e9b2faf9481794f40cafff4a8fcbbc15
Andrew McMurtrie <cmp-202@anyzoom.com>
2016-11-09 06:27:26 +1300

    data
    added on data example ti pipe.js test file

--- test/pipe.js
+++ test/pipe.js
@@ -1,14 +1,13 @@
 var dotenv = require('dotenv'),
     fs = require('fs')
-dotenv.load();
+dotenv.load()
 
 var host = {
   server:             {     
     host:         process.env.HOST,
     port:         process.env.PORT,
     userName:     process.env.USER_NAME,
-    password:     process.env.PASSWORD,
-    tryKeyboard:  true
+    password:     process.env.PASSWORD
   },
   commands:           [
     "`Test session text message: passed`",
@@ -16,20 +15,26 @@ var host = {
     "ls -la"
   ],
 
-};
+}
 //until npm published use the cloned dir path.
-var SSH2Shell = require ('../lib/ssh2shell');
+var SSH2Shell = require ('../lib/ssh2shell')
 
 //run the commands in the shell session
 var SSH = new SSH2Shell(host),
     callback = function( sessionText ){
           console.log ( "-----Callback session text:\n" + sessionText);
           console.log ( "-----Callback end" );
-      }
-var firstLog = fs.createWriteStream('first.log'),
-    secondLog = fs.createWriteStream('second.log');
+      },
+    firstLog = fs.createWriteStream('first.log'),
+    secondLog = fs.createWriteStream('second.log'),
+    buffer = ""
 
 //multiple pipes can be added but they wont be bound to the stream until the connection is established    
 SSH.pipe(firstLog).pipe(secondLog);    
+
+SSH.on('data', function(data){
+    //do something with the data chunk
+    console.log(data)
+})
 
 SSH.connect(callback)
