e1923245ebb1f64d876894e56707e62b14152a6a
Andrew McMurtrie <cmp-202@anyzoom.com>
2016-11-09 21:49:45 +1300

    simple.js
    Removed redundent host.sever option. Added a commented require statement
    for `ssh2shell` showing the difference between path and package require.

--- test/simple.js
+++ test/simple.js
@@ -7,8 +7,7 @@ var host = {
     host:         process.env.HOST,
     port:         process.env.PORT,
     userName:     process.env.USER_NAME,
-    password:     process.env.PASSWORD,
-    tryKeyboard:  true
+    password:     process.env.PASSWORD
   },
   commands:           [
     "`Test session text message: passed`",
@@ -17,7 +16,8 @@ var host = {
   ],
 
 };
-//until npm published use the cloned dir path.
+
+//var SSH2Shell = require ('ssh2shell');
 var SSH2Shell = require ('../lib/ssh2shell');
 
 //run the commands in the shell session
