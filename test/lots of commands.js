55a37642bfc834c208c212ba76b2e75d73c86ccc
cmp-202 <cmp-202@anyzoom.com>
2022-06-30 12:18:30 +1200

    Create lots of commands.js
    Testing effect on listeners count when running lots of commands.

---
+++ test/lots of commands.js
@@ -0,0 +1,87 @@
+var dotenv = require('dotenv').config(),
+    fs = require('fs')
+
+var host = {
+  server:             {     
+    host:         process.env.HOST,
+    port:         process.env.PORT,
+    userName:     process.env.USER_NAME,
+    password:     process.env.PASSWORD
+  },
+  commands:           [
+    "msg: first",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "msg: 11",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "msg: 21",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update",
+    "sudo apt-get -y update"
+  ]
+};
+//until npm published use the cloned dir path.
+var SSH2Shell = require ('../lib/ssh2shell');
+
+//run the commands in the shell session
+var SSH = new SSH2Shell(host),
+locLog = fs.createWriteStream('Lots of commands.log')
+
+SSH.pipe( locLog );
+SSH.connect();
